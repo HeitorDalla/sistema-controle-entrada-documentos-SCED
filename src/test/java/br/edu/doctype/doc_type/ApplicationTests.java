0f38f28ca419b0ced63de418b245c6ecba2287dc
HeitorDalla <heitorvillavilla@gmail.com>
2026-04-01 21:58:18 -0300

    chore: inicializacao do projeto

---
+++ src/test/java/br/edu/doctype/doc_type/ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.doctype.doc_type;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
